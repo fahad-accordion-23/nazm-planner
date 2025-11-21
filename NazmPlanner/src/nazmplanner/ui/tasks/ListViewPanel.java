68f57be3f0dfc9430bc74edd2db4654c674ee8f2
Fahad Hassan <fahad.1979.hassan@gmail.com>
2025-11-21 19:26:39 +0500

    Add layout and skeleton for Tasks UI: PrimaryPanel
    - Adds a ListViewPanel
    - Adds a CreationFormPanel
    - Sets the layout for PrimaryPanel, adding the above panels

---
+++ NazmPlanner/src/nazmplanner/ui/tasks/ListViewPanel.java
@@ -0,0 +1,8 @@
+package nazmplanner.ui.tasks;
+
+import javax.swing.JPanel;
+
+public class ListViewPanel extends JPanel
+{
+
+}
--- NazmPlanner/src/nazmplanner/ui/tasks/PrimaryPanel.java
+++ NazmPlanner/src/nazmplanner/ui/tasks/PrimaryPanel.java
@@ -1,7 +1,12 @@
 package nazmplanner.ui.tasks;
 
 import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import nazmplanner.ui.components.HeaderPanel;
+import nazmplanner.ui.util.GBC;
 
 /**
  * <h2>PrimaryPanel</h2>
@@ -14,13 +19,46 @@ import javax.swing.JPanel;
  */
 public class PrimaryPanel extends JPanel
 {
+    private HeaderPanel headerPanel;
+    private ListViewPanel listViewPanel;
+    private JScrollPane listViewScrollPane;
+    private CreationFormPanel creationFormPanel;
+    
     public PrimaryPanel()
     {
+        headerPanel = new HeaderPanel("Tasks");
+        listViewPanel = new ListViewPanel();
+        listViewScrollPane = new JScrollPane(listViewPanel);
+        creationFormPanel = new CreationFormPanel();
+        
         initStyling();
+        initLayout();
     }
     
     private void initStyling()
     {
         super.setBackground(Color.DARK_GRAY);
+    }
+    
+    private void initLayout()
+    {
+        super.setLayout(new GridBagLayout());
+        
+        super.add(headerPanel, 
+                  new GBC(0, 0, 1, 0)
+                  .setWeight(1.00, 0.00)
+                  .setAnchor(GridBagConstraints.NORTH)
+                  .setFill(GridBagConstraints.HORIZONTAL));
+        
+        super.add(listViewScrollPane,
+                  new GBC(0, 1, 1, 0)
+                  .setWeight(1.00, 1.00)
+                  .setFill(GridBagConstraints.BOTH));
+        
+        super.add(creationFormPanel,
+                  new GBC(0, 0, 2, 0)
+                  .setWeight(1.00, 0.00)
+                  .setAnchor(GridBagConstraints.SOUTH)
+                  .setFill(GridBagConstraints.HORIZONTAL));
     }
 }
