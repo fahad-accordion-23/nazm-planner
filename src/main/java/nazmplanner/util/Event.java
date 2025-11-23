59086225eb9c1c07373a0716476166844a76e8b2
Fahad Hassan <fahad.1979.hassan@gmail.com>
2025-11-24 04:10:07 +0500

    Implement generic EventBroker system

---
+++ src/main/java/nazmplanner/util/Event.java
@@ -0,0 +1,14 @@
+package nazmplanner.util;
+
+/**
+ * <h2>Event</h2>
+ * 
+ * <p>Represents an event/context</p>
+ * 
+ * @author Fahad Hassan
+ * @version 24/11/25
+ */
+public interface Event
+{
+
+}
